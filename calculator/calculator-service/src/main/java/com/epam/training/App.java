85a8b5a1877e5c19ad4fdcce963e5555ce33473a
Endre Ferencz <Endre_Ferencz@epam.com>
2015-06-18 16:49:24 +0200

    first commit

---
+++ calculator/calculator-service/src/main/java/com/epam/training/App.java
@@ -0,0 +1,18 @@
+package com.epam.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int add(int a, int b) {
+	return a + b;
+	
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ calculator/calculator-web/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>
+
+<%= com.epam.training.App.add(1,2) %>
+
+</h2>
+</body>
+</html>
